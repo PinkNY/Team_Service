0c33ea7045c8e108c14da4abd5ab012c2c3665fd
zxc <altoranah@naver.com>
2024-10-19 11:22:31 +0000

    check

--- frontend/src/pages/styles/MainSt.js
+++ frontend/src/pages/styles/MainSt.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { media } from '../MediaQ';
 
 export const PageWrapper = styled.div`
   min-height: 100vh;
@@ -18,10 +19,11 @@ export const MainContent = styled.main`
 `;
 
 export const SearchInput = styled.input`
-  width: 100%;
-  padding: 0.5rem 1rem 0.5rem 2.5rem;
+  width: 80%;
+  padding: 0.5rem 1rem 0.5rem 0.5rem;
   border-radius: 9999px;
   border: 2px solid #3b82f6;
+  font-size: 15px;
   &:focus {
     outline: none;
     box-shadow: 0 0 0 3px rgba(59, 130, 246, 0.5);
@@ -30,7 +32,8 @@ export const SearchInput = styled.input`
 
 export const Button = styled.button`
   padding: 0.5rem 1rem;
-  border-radius: 0.25rem;
+  border: 1px solid #3b82f6;
+  border-radius: 15px;
   font-weight: 500;
   cursor: pointer;
   transition: background-color 0.2s;
@@ -87,19 +90,61 @@ export const ChatbotWindow = styled.div`
   padding: 1rem;
   display: flex;
   flex-direction: column;
+
+  ${media.mobile} {
+    width: 70vw;
+    height: 60vh;
+  }
 `;
 
 export const ChatbotMessages = styled.div`
   flex-grow: 1;
   overflow-y: auto;
   margin-bottom: 1rem;
-  background-color: #f9fafb;
+  background-color: white;
   padding: 0.5rem;
   border-radius: 0.25rem;
+`;
+
+// 새롭게 추가된 말풍선 스타일
+export const ChatBubble = styled.div`
+  position: relative;
+  background: #f1f0f0; /* 말풍선 배경색 */
+  border-radius: 15px;
+  padding: 10px 15px;
+  margin-bottom: 10px;
+  max-width: 80%;
+  color: #333;
+  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1);
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 10px;
+    left: -10px;
+    width: 0;
+    height: 0;
+    border: 10px solid transparent;
+    border-right-color: #f1f0f0;
+    border-left: 0;
+    margin-top: 0;
+  }
+`;
+
+export const SendWindow = styled.div`
+  display: flex;
+  width: 100%;
+  gap: 10px;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const HalfBox = styled.div`
   display: flex;
   justify-content: center;
-  gap: 50px;
+  gap: 40px;
+
+  ${media.mobile} {
+    flex-direction: column;
+  }
 `;
